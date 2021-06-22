1f6887e15ed2e4d450b2ec041617ab736c0df2cf
Andrew Burgess <andrew.burgess@embecosm.com>
2021-06-22 11:26:28 +0100

    [wip] gcc: generate nested function trampolines off stack
    An initial attempt to support nested functions on Darwin/AArch64,
    where the stack is marked non-execute.
    
    Nested function trampolines are allocated into off-stack pages, which
    are marked as executable.  The trampolines are deallocated using a
    finally block in the scope of the function containing the nested
    function.
    
    The only problem with this is that nested function trampolines will be
    leaked if a user longjmps over the deallocation code (as the finally
    block will not be executed).  We could potentially look to solve this
    problem by hooking into the setjmp/longjmp code within GCC, and
    trying to understand which trampolines have been jumped over (and so
    need releasing), however, for now I'm considering that a separate,
    second stage of the project.
    
    The current code is very much work in progress, there's a mountain of
    things that still need doing:
    
    (1) The choice between on-stack and off-stack trampolines is currently
    controlled by a global in tree-nested.c, this is obviously just a
    temporary back, and needs a real control option, but...
    
    (2) ... I suspect some of the on-stack trampoline code may have been
    broken by my hacking in the off-stack trampoline support, so we need
    to audit all of the code changes and make sure that I've not broken
    the traditional way of doing things.
    
    (3) The current library code to allocate and deallocate trampolines
    needs to be moved to a new home, it probably also needs to be
    configure-time selectable, as targets that have an executable stack
    are unlikely to want to use the off-stack approach (at least for now).
    
    (4) The library code needs to be updated in two respects:
    
        (a) we need to support generating more than a single page of
        trampolines, this will require us to be able to allocate
        additional pages and chain them together, then of course, we
        should possibly be deallocating pages as trampolines are released,
        and
    
        (b) we should generate trampolines by copying in some template
        code, and then patching it with the appropriate addresses, this is
        not what we currently do.

--- libgcc/unwind-c.c
+++ libgcc/unwind-c.c
@@ -30,6 +30,8 @@ see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
 #include "unwind.h"
 #define NO_SIZE_OF_ENCODED_VALUE
 #include "unwind-pe.h"
+#include <sys/mman.h>
+#include <stdint.h>
 
 typedef struct
 {
@@ -244,3 +246,169 @@ __gcc_personality_seh0 (PEXCEPTION_RECORD ms_exc, void *this_frame,
 				ms_disp, __gcc_personality_imp);
 }
 #endif /* SEH */
+
+
+extern void __builtin_nested_func_ptr_created (void *sp, void *chain, void *func, void **dst);
+extern void __builtin_nested_func_ptr_deleted (void *sp, void *chain, void *func, void **dst);
+
+struct tramps_ctrl_data
+{
+  void *start_of_first_page;
+  void *current_ptr;
+};
+
+static _Thread_local struct tramps_ctrl_data *tramps_ctrl = NULL;
+
+static struct tramps_ctrl_data *
+tramps_init_ctrl_data (void)
+{
+  int page_size;
+
+  /* TODO: We are leaking memory on failure here.  */
+
+  struct tramps_ctrl_data *p = malloc (sizeof (struct tramps_ctrl_data));
+  if (p == NULL)
+    return NULL;
+  memset (p, 0, sizeof (struct tramps_ctrl_data));
+
+  /* Allocate the first page.  */
+  page_size = getpagesize ();
+  p->start_of_first_page = mmap (0, page_size, PROT_WRITE | PROT_EXEC,
+				 MAP_ANON | MAP_PRIVATE, 0, 0);
+  if (p->start_of_first_page == MAP_FAILED)
+    return NULL;
+
+  p->current_ptr = p->start_of_first_page;
+
+  return p;
+}
+
+static void
+write_byte (uintptr_t addr, int val)
+{
+  unsigned char *ptr = (unsigned char *) addr;
+  *ptr = (val & 0xff);
+}
+
+/* Size of a single trampoline entry, in bytes.  */
+#define SIZE_OF_TRAMPOLINE 64
+
+void
+__builtin_nested_func_ptr_created (void *sp, void *chain, void *func, void **dst)
+{
+  uintptr_t ptr, addr, start, end;
+  int page_size;
+
+  if (tramps_ctrl == NULL)
+    tramps_ctrl = tramps_init_ctrl_data ();
+  if (tramps_ctrl == NULL)
+    abort ();	/* TODO: Something better?  */
+
+  /* Figure out the start, and then the end, of the page currently being written
+     too.  */
+  page_size = getpagesize ();
+  ptr = (uintptr_t) tramps_ctrl->current_ptr;
+  start = ptr & ~(page_size - 1);
+  end = start + page_size;
+  printf ("Current trampoline page, start = %p, end = %p\n",
+	  ((void *) start), ((void *) end));
+
+  /* TODO: Eventually, this check needs to trigger the allocation of another
+     page of memory, and then somehow link that new page in with the previous
+     pages.  */
+  if (ptr + SIZE_OF_TRAMPOLINE >= end)
+    abort ();
+
+  /* 1. Generate code for the trampoline, filling in those bits as required from
+     the data passed into this function.  */
+  ptr = (uintptr_t) tramps_ctrl->current_ptr;
+  write_byte (ptr++, 0x41);
+  write_byte (ptr++, 0xbb);
+  addr = (uintptr_t) func;
+  write_byte (ptr++, (addr & 0xff));
+  addr >>= 8;
+  write_byte (ptr++, (addr & 0xff));
+  addr >>= 8;
+  write_byte (ptr++, (addr & 0xff));
+  addr >>= 8;
+  write_byte (ptr++, (addr & 0xff));
+  addr >>= 8;
+
+  write_byte (ptr++, 0x49);
+  write_byte (ptr++, 0xba);
+  addr = (uintptr_t) chain;
+  write_byte (ptr++, (addr & 0xff));
+  addr >>= 8;
+  write_byte (ptr++, (addr & 0xff));
+  addr >>= 8;
+  write_byte (ptr++, (addr & 0xff));
+  addr >>= 8;
+  write_byte (ptr++, (addr & 0xff));
+  addr >>= 8;
+  write_byte (ptr++, (addr & 0xff));
+  addr >>= 8;
+  write_byte (ptr++, (addr & 0xff));
+  addr >>= 8;
+  write_byte (ptr++, (addr & 0xff));
+  addr >>= 8;
+  write_byte (ptr++, (addr & 0xff));
+  addr >>= 8;
+
+  write_byte (ptr++, 0x49);
+  write_byte (ptr++, 0xff);
+  write_byte (ptr++, 0xe3);
+
+  write_byte (ptr++, 0x90);
+  write_byte (ptr++, 0x90);
+  write_byte (ptr++, 0x90);
+  write_byte (ptr++, 0x90);
+  write_byte (ptr++, 0x90);	/* 24-bytes.  */
+
+  /* 2. Flush the i-cache for the code we just wrote.  We're going to also write
+     some data onto this page, but we don't need to flush the icache for
+     that.*/
+  __builtin___clear_cache (tramps_ctrl->current_ptr, ptr);
+
+  /* 3. Write out the information into a header block so we can understand what
+     this trampoline represents.  */
+  ptr += 24;	/* Leave a gap.  */
+  *((void **) ptr) = (void *) chain;
+  ptr += 8;
+  *((void **) ptr) = (void *) func;
+  ptr += 8;
+
+  /* 4. Return a pointer to the new trampoline.  */
+  *dst = tramps_ctrl->current_ptr;
+  tramps_ctrl->current_ptr = ptr;
+
+  printf ("GCC: Generating a nested function pointer\n");
+  printf ("     sp = %p, chain = %p, func = %p, dst = %p\n", sp, chain, func, dst);
+  printf ("     ctrl = %p\n", tramps_ctrl);
+  printf ("     tramp = %p\n", tramps_ctrl->current_ptr);
+}
+
+void
+__builtin_nested_func_ptr_deleted (void *sp, void *chain, void *func, void **dst)
+{
+  uintptr_t ptr, addr, start, end;
+  int page_size;
+
+  /* If the control structure is not initialised then surely there should be no
+     trampolines allocated.  So, why did we end up here at all?  */
+  if (tramps_ctrl == NULL)
+    abort ();		/* Maybe we should do something better.  */
+
+  /* Figure out the start, and then the end, of the page currently being written
+     too.  */
+  page_size = getpagesize ();
+  ptr = (uintptr_t) tramps_ctrl->current_ptr;
+  start = ptr & ~(page_size - 1);
+  end = start + page_size;
+  printf ("Current trampoline page, start = %p, end = %p\n",
+	  ((void *) start), ((void *) end));
+
+  void *tramp = *dst;
+  printf ("GCC: Deleting a nested function pointer\n");
+  printf ("     sp = %p, chain = %p, func = %p, tramp = %p\n", sp, chain, func, tramp);
+  printf ("     ctrl = %p\n", tramps_ctrl);
+}
